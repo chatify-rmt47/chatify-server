3070a5342024b4fef21eb57ad6b78facc178967a
habibmufti <19102170@ittelkom-pwt.ac.id>
2024-04-23 16:28:25 +0700

    feat: add error handler

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const router = require('./routes');
-const connectToMongoDB = require('./db/connectToMongoDB');
-const ErrorHandler = require('./middleware/ErrorHandler');
+require("dotenv").config();
+const express = require("express");
+const router = require("./routes");
+const connectToMongoDB = require("./db/connectToMongoDB");
+const ErrorHandler = require("./middleware/ErrorHandler");
 const app = express();
 const port = process.env.PORT || 3000;
 
