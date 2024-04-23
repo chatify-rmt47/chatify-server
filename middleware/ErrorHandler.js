d5e1eca206a8e3121ea74f7c5d4c6c270766e3e2
habibmufti <19102170@ittelkom-pwt.ac.id>
2024-04-23 16:28:25 +0700

    feat: add error handler

--- middleware/ErrorHandler.js
+++ middleware/ErrorHandler.js
@@ -2,36 +2,36 @@ let ErrorHandler = async (err, req, res, next) => {
   let name = err.name;
   let message = err.message;
 
-  if (message === 'jwt malformed' || message === 'invalid signature') {
-    res.status(401).json({ message: 'Error authentication' });
+  if (message === "jwt malformed" || message === "invalid signature") {
+    res.status(401).json({ message: "Error authentication" });
     return;
   }
 
   switch (name) {
-    case 'validateError':
+    case "validateError":
       res.status(400).json({ message });
       break;
-    case 'invalidInput':
+    case "invalidInput":
       res.status(400).json({ message });
       break;
-    case 'validationError':
+    case "validationError":
       res.status(401).json({ message });
       break;
-    case 'invalidToken':
-      res.status(401).json({ message: 'Error authentication' });
+    case "invalidToken":
+      res.status(401).json({ message: "Error authentication" });
       break;
-    case 'forbidden':
-      res.status(403).json({ message: 'Forbidden' });
+    case "forbidden":
+      res.status(403).json({ message: "Forbidden" });
       break;
-    case 'MissingFile':
+    case "MissingFile":
       res.status(400).json({ message });
       break;
-    case 'forbidden':
-      res.status(403).json({ message: 'Forbidden' });
+    case "forbidden":
+      res.status(403).json({ message: "Forbidden" });
       break;
     default:
       res.status(500).json({
-        message: 'Internal server error',
+        message: "Internal server error",
       });
       break;
   }
