a5892558e35a1fa9eed05e5c49c9813b33a3b3ed
habibmufti <19102170@ittelkom-pwt.ac.id>
2024-04-23 19:48:20 +0700

    feat(app.js): add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const router = require("./routes");
 const connectToMongoDB = require("./db/connectToMongoDB");
 const ErrorHandler = require("./middleware/ErrorHandler");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
