a6a45786c1e597aada5808d61ac0c0614de0eaa7
habibmufti <19102170@ittelkom-pwt.ac.id>
2024-04-24 15:57:56 +0700

    feat: socket.io

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const router = require("./routes");
 const connectToMongoDB = require("./db/connectToMongoDB");
 const ErrorHandler = require("./middleware/ErrorHandler");
-const app = express();
+const { app, server } = require("./socket/socket");
 const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(router);
 app.use(ErrorHandler);
-app.listen(port, () => {
+server.listen(port, () => {
   connectToMongoDB();
   console.log(`Example app listening on port ${port}!`);
 });
