cd9f2ee7be7ccb0bf3e43de1df816d3e5bb8a89d
Nathaniel Kevin <nathaniel.kevin.untoro@gmail.com>
2024-04-24 21:20:56 +0700

    update: adding validation for config when doing development

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const router = require("./routes");
-const connectToMongoDB = require("./db/connectToMongoDB");
-const ErrorHandler = require("./middleware/ErrorHandler");
-const { app, server } = require("./socket/socket");
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const router = require('./routes');
+const connectToMongoDB = require('./db/connectToMongoDB');
+const ErrorHandler = require('./middleware/ErrorHandler');
+const { app, server } = require('./socket/socket');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
